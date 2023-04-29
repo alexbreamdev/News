aef92e06e49b4adb05797a0367695eb47651e915
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-04-29 12:47:47 +0300

    initial commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Oleksii Leshchenko on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
